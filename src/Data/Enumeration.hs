489ea8d56ac4dbe10e9a4f4278b099e5e6dfe2d1
Eric McCorkle <emc2@metricspace.net>
2014-09-20 11:25:57 -0400

    Completed basic enumeration functionality

--- src/Data/Enumeration.hs
+++ src/Data/Enumeration.hs
@@ -27,51 +27,175 @@
 -- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 -- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 -- SUCH DAMAGE.
-{-# OPTIONS_GHC -Wall -Werror #-}
+{-# OPTIONS_GHC -Wall -Werror -funbox-strict-fields #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 
 module Data.Enumeration(
        -- * Definitions
        Enumeration,
-       Path
-       {-
-       BadPath,
+       Path,
+       BadPath(..),
 
        -- ** Using Enumerations
        fromPath,
-       toPath
+       toPath,
+       withPrefix,
+       numBranches,
+       prefix,
 
        -- * Constructions
+       singleton,
+       singletonWithPrefix,
        fromEncoding,
-       step
--}
+       fromEncodingWithPrefix,
+       step,
+       stepWithPrefix
        ) where
 
-import Data.ArithEncode
+import Control.Exception
+import Data.List
+import Data.ArithEncode hiding (singleton)
+import Data.Typeable
 
+-- | A path that uniquely identifies a value in an @Enumeration@.
 type Path = [Integer]
 
-data Enumeration ty1 ty2 =
-    End {
-      endEnc :: Encoding ty2
-    }
-  | Step {
-      stepEnc :: Encoding ty1,
-      stepNext :: ty1 -> Enumeration ty ty2
+-- | A datatype that represents a mapping between @Path@s and @ty@s.
+-- Note that unlike @Encoding@s, not all @Path@s are necessarily
+-- valid.
+data Enumeration ty =
+  Enumeration {
+    -- | Convert a @ty@ to a @Path@
+    toPath :: !(ty -> Path),
+    -- | Generate an @ty@ from a @Path@
+    fromPath :: !(Path -> ty),
+    -- | Given a prefix path, get an enumeration that generates @ty@s
+    -- from the rest of the path.
+    withPrefix :: !(Path -> Enumeration ty),
+    -- | Get the upper bound on values for the first path component,
+    -- or @Nothing@ if there is no bound.
+    numBranches :: !(Maybe Integer),
+    -- | The reversed prefix path.
+    prefix :: !Path
   }
-{-
--- | Generate an @Enumeration@ from a single @Encoding@.  The @Path@
--- will always be of length 1, and contains the encoded value.
-fromEncoding :: Encoding ty -> Enumeration ty
-fromEncoding enc =
-  let
-    frompathfunc [ encoded ] = decode enc encoded
-    topathfunc val = [ encode enc val ]
-  in
-    Enumeration { fromPath = frompathfunc, toPath = topathfunc }
 
+data BadPath = BadPath String
+  deriving Typeable
+
+instance Show BadPath where
+  show (BadPath "") = "Bad Path"
+  show (BadPath msg) = "Bad Path: " ++ msg
+
+instance Exception BadPath
+
+showPath :: Path -> String
+showPath = intercalate "." . map show
+
+-- | Create an @Enumeration@ with an empty prefix that maps a single
+-- value to and from the empty path.  Equivalent to
+-- @singletonWithPrefix []@
+singleton :: Eq ty =>
+             ty
+          -- ^ The value to map to and from the empty path.
+          -> Enumeration ty
+singleton = singletonWithPrefix []
+
+-- | Create an @Enumeration@ with a given prefix path that maps a
+-- single value to and from the empty path.
+singletonWithPrefix :: Eq ty => Path -> ty -> Enumeration ty
+singletonWithPrefix prefixPath val =
+  let
+    showCompletePath path = showPath (prefixPath ++ path)
+
+    fromPathFunc [] = val
+    fromPathFunc path =
+      throw (BadPath ("Extra path elements " ++ showCompletePath path))
+
+    toPathFunc val'
+      | val' == val = []
+      | otherwise = throw (IllegalArgument "Bad argument to singleton")
+
+    withPrefixFunc [] = out
+    withPrefixFunc path =
+      throw (BadPath ("Extra path elements " ++ showCompletePath path))
+
+    out = Enumeration { fromPath = fromPathFunc, toPath = toPathFunc,
+                        withPrefix = withPrefixFunc, numBranches = Just 0,
+                        prefix = prefixPath }
+  in
+    out
+
+-- | Create an @Enumeration@ with an empty prefix from a single
+-- @Encoding@.  The @Path@ will always be of length 1, and contains
+-- the encoded value.
+fromEncoding :: Eq ty =>
+                Encoding ty
+             -- ^ The @Encoding@ to use
+             -> Enumeration ty
+fromEncoding = fromEncodingWithPrefix []
+
+-- | Create an @Enumeration@ with a given prefix from a single
+-- @Encoding@.  The @Path@ will always be of length 1, and contains
+-- the encoded value.
+fromEncodingWithPrefix :: Eq ty => Path -> Encoding ty -> Enumeration ty
+fromEncodingWithPrefix prefixPath enc =
+  let
+    fromPathFunc [ encoded ] = decode enc encoded
+    fromPathFunc [] = throw (BadPath "Path too short")
+    fromPathFunc (_ : path) =
+      throw (BadPath ("Extra path elements " ++ showPath path))
+
+    toPathFunc val = [ encode enc val ]
+
+    withPrefixFunc [ encoded ] = singleton (decode enc encoded)
+    withPrefixFunc [] = throw (BadPath "Path too short")
+    withPrefixFunc (_ : path) =
+      throw (BadPath ("Extra path elements " ++ showPath path))
+  in
+    Enumeration { fromPath = fromPathFunc, toPath = toPathFunc,
+                  withPrefix = withPrefixFunc, numBranches = size enc,
+                  prefix = prefixPath }
+
+-- | Create an @Enumeration@ with an empty prefix that uses an
+-- @Encoding@ to convert the first element of the path to an interim
+-- value, then uses that value to construct an @Enumeration@ to decode
+-- the rest of the path.
 step :: Encoding ty1
      -- ^ The encoding for the first type.
      -> (ty1 -> Enumeration ty2)
      -- ^ A function that produces an enumeration from the first type.
+     -> (ty2 -> ty1)
+     -- ^ A function that extracts the first type from the second.
      -> Enumeration ty2
--}
+step = stepWithPrefix []
+
+-- | Create an @Enumeration@ with an empty prefix that uses an
+-- @Encoding@ to convert the first element of the path to an interim
+-- value, then uses that value to construct an @Enumeration@ to decode
+-- the rest of the path.
+stepWithPrefix :: Path
+               -- ^ The prefix path.
+               -> Encoding ty1
+               -- ^ The encoding for the first type.
+               -> (ty1 -> Enumeration ty2)
+               -- ^ A function that produces an enumeration from the first type.
+               -> (ty2 -> ty1)
+               -- ^ A function that extracts the first type from the second.
+               -> Enumeration ty2
+stepWithPrefix prefixPath enc build extract =
+  let
+    fromPathFunc (first : rest) = fromPath (build (decode enc first)) rest
+    fromPathFunc [] = throw (BadPath "Path too short")
+
+    toPathFunc val =
+      let
+        extracted = extract val
+      in
+        encode enc extracted : toPath (build extracted) val
+
+    withPrefixFunc (first : rest) = withPrefix (build (decode enc first)) rest
+    withPrefixFunc [] = throw (BadPath "Path too short")
+  in
+   Enumeration { fromPath = fromPathFunc, toPath = toPathFunc,
+                 withPrefix = withPrefixFunc, numBranches = size enc,
+                 prefix = prefixPath }
